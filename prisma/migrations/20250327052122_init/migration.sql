22904f33efe2c6a46908a44f6936fe8447a6b357
sobhan-yaghoubi <sobhan.yaghoubi.work@gmail.com>
2025-03-27 16:27:08 +0330

    build => add prisma to project

---
+++ prisma/migrations/20250327052122_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
